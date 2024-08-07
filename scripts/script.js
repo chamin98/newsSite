f49e93558d3ca3cc74b5658b975e2f078d6ffe18
cs_tg <61661109+chamin98@users.noreply.github.com>
2024-08-07 17:07:54 +0530

    All without serach function

---
+++ scripts/script.js
@@ -0,0 +1,67 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const apikey = "daf2e558d9234514b40763f8bcb8ecb1";
+  const blogContainer = document.getElementById("blog-container");
+
+  if (!blogContainer) {
+    console.error("Error: blogContainer element not found.");
+    return;
+  }
+
+  async function fetchRandomNews() {
+    try {
+      const apiUrl = `https://newsapi.org/v2/top-headlines?sources=techcrunch&pageSize=15&apikey=${apikey}`;
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      return data.articles;
+    } catch (error) {
+      console.error("Error fetching", error);
+      return [];
+    }
+  }
+
+  function displayBlogs(articles) {
+    blogContainer.innerHTML = "";
+    articles.forEach((article) => {
+      const blogCard = document.createElement("div");
+      blogCard.classList.add("blog-card");
+
+      const img = document.createElement("img");
+      img.src = article.urlToImage || "https://placehold.co/600x400"; // Fallback image URL
+      img.alt = article.title;
+
+      const title = document.createElement("h2");
+      const truncatedTitle =
+        article.title.length > 30
+          ? article.title.slice(0, 30) + "....."
+          : article.title;
+      title.textContent = truncatedTitle;
+
+      const description = document.createElement("p");
+      
+
+      const truncatedDes =
+      article.description.length > 120
+        ? article.description.slice(0, 120) + "....."
+        : article.description;
+        description.textContent = truncatedDes;
+
+
+      blogCard.appendChild(img);
+      blogCard.appendChild(title);
+      blogCard.appendChild(description);
+      blogCard.addEventListener("click", () =>{
+        window.open(article.url, "_blank")
+      })
+      blogContainer.appendChild(blogCard);
+    });
+  }
+
+  (async () => {
+    try {
+      const articles = await fetchRandomNews();
+      displayBlogs(articles);
+    } catch (error) {
+      console.error("Error fetching", error);
+    }
+  })();
+});
